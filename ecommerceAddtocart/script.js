ad2a1cbfcfadf49a10080b494aa71351e263584b
sanish <s.karki1994@gmail.com>
2024-04-24 17:14:19 +0545

    price increase/decrease according to quantity

--- ecommerceAddtocart/script.js
+++ ecommerceAddtocart/script.js
@@ -1,23 +1,28 @@
 // 1) ------ Increment/Decrement ------
 
-const decreaseNumber = () => {
-  var itemval = document.getElementById('textbox');
-  // console.log(itemval.value);
+const decreaseNumber = (incdec, itemprice) => {
+  var itemval = document.getElementById(incdec);
+  var itemprice = document.getElementById(itemprice);
 
   if (itemval.value <= 0) {
     //yo gare paxi value -ve ma jadaina
     itemval.value = 0;
+    alert('Negetive quantity not allowed!');
   } else {
     itemval.value = parseInt(itemval.value) - 1;
 
     itemval.style.background = '#fff';
     itemval.style.color = '#000';
+
+    // --Price +/- according to quantity
+    itemprice.innerHTML = parseInt(itemprice.innerHTML) - 15;
   }
 };
 
-const increaseNumber = () => {
-  var itemval = document.getElementById('textbox');
- 
+const increaseNumber = (incdec, itemprice) => {
+  var itemval = document.getElementById(incdec);
+  var itemprice = document.getElementById(itemprice);
+
   if (itemval.value >= 5) {
     itemval.value = 5;
 
@@ -26,35 +31,10 @@ const increaseNumber = () => {
     itemval.style.color = '#fff';
   } else {
     itemval.value = parseInt(itemval.value) + 1;
+
+    // --Price +/- according to quantity
+    itemprice.innerHTML = parseInt(itemprice.innerHTML) + 15;
   }
 };
 
 // -----------------------PRATICE--------------------------------
-
-// const decreaseNumber = () => {
-//   let itemval = document.getElementById('textbox');
-//   console.log(itemval.value);
-
-//   if (itemval.value <= 0) {
-//     itemval.value = 0;
-//   } else {
-//     itemval.value = parseInt(itemval.value) - 1;
-
-//     itemval.style.background = '#fff';
-//     itemval.style.color = '#000';
-//   }
-// };
-
-// const increaseNumber = () => {
-//   let itemval = document.getElementById('textbox');
-
-//   if (itemval.value >= 5) {
-//     itemval.value = 5;
-
-//     alert('Maxiumum 5 allowed!');
-//     itemval.style.background = 'red';
-//     itemval.style.color = '#fff';
-//   } else {
-//     itemval.value = parseInt(itemval.value) + 1;
-//   }
-// };
