f2545688244f7014234653a62a80ed1d0fc83d33
sanish <s.karki1994@gmail.com>
2024-04-25 16:48:22 +0545

    praticed

--- ecommerceAddtocart/script.js
+++ ecommerceAddtocart/script.js
@@ -59,3 +59,60 @@ const increaseNumber = (incdec, itemprice) => {
 };
 
 // -----------------------PRATICE--------------------------------
+
+// var product_total_amt = document.getElementById('product_total_amt');
+// var total_cart_amt = document.getElementById('total_cart_amt');
+// var shipping_charge = document.getElementById('shipping_charge');
+
+// const decreaseNumber = (incdec, itemprice) => {
+//   var itemval = document.getElementById(incdec);
+//   var itemprice = document.getElementById(itemprice);
+
+//   if (itemval.value <= 0) {
+//     itemval.value = 0;
+
+//     alert('No negetive quantity');
+//   } else {
+//     itemval.value = parseInt(itemval.value) - 1;
+
+//     itemval.style.background = '#fff';
+//     itemval.style.color = '#000';
+
+//     // --Price +/- according to quantity
+//     itemprice.innerHTML = parseInt(itemprice.innerHTML) - 15;
+
+//     // -- Total amt after adding quantity
+//     product_total_amt.innerHTML = parseInt(product_total_amt.innerHTML) - 15;
+
+//     // --Total amt after shipping charge
+//     total_cart_amt.innerHTML =
+//       parseInt(product_total_amt.innerHTML) +
+//       parseInt(shipping_charge.innerHTML);
+//   }
+// };
+
+// const increaseNumber = (incdec, itemprice) => {
+//   var itemval = document.getElementById(incdec);
+//   var itemprice = document.getElementById(itemprice);
+
+//   if (itemval.value >= 5) {
+//     itemval.value = 5;
+
+//     alert('Maximum 5 allowed');
+//     itemval.style.background = 'red';
+//     itemval.style.color = '#fff';
+//   } else {
+//     itemval.value = parseInt(itemval.value) + 1;
+
+//     // --Price +/- according to quantity
+//     itemprice.innerHTML = parseInt(itemprice.innerHTML) + 15;
+
+//     // -- Total amt after adding quantity
+//     product_total_amt.innerHTML = parseInt(product_total_amt.innerHTML) + 15;
+
+//     // --Total amt after shipping charge
+//     total_cart_amt.innerHTML =
+//       parseInt(product_total_amt.innerHTML) +
+//       parseInt(shipping_charge.innerHTML);
+//   }
+// };
